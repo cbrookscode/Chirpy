1624f646db5a61e0a0b7cc2717a273721fd57189
cbrookscode <colbybrooks@myyahoo.com>
2025-06-06 12:19:06 -0400

    added /metrics and /reset routing logic which will allow tracking for number of visits on the page.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Chirpy
+
+## Mux.Handle vs mux.HandleFunc
+- Handle requires that the second arguement implements the http.Handler interface where handlefunc does not. Handlefunc will take the function provided and make it impelemnt the http.Handler interface. Handle is used for when you want to implement a custom router, middleware, or other custom logic. If you want to make a function into a http.Handler then you can use the http.HanlderFunc() function to do so. Ex: middlewareMetricsInc method on apiConfig.
+
+## Middleware logic and why.
+- When these handlers are registered with mux it calls the func. This is why you need to make sure the logic for the incrementing of metrics is handled inside of the anonymous function return instead of the body of the function. If the increment happend in the body then it would happen once on startup then never again. This function will now instead return a new http.Handler that will get registered and then when it is called will increment the counter correctly. This new handler just icnrements the counter and calls servhttp against the original app handler for the normal website.
--- main.go
+++ main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-// Setting up handler to match expected signature for http.HandlerFunc
-func myhandler(reswriter http.ResponseWriter, webrequest *http.Request) {
+// in memory struct to keep track of apidata
+type apiConfig struct {
+	fileserverhits atomic.Int32
+}
+
+// wrapper for app handler so that we can increment filserverhits accurately.
+func (state *apiConfig) middlewareMetricsInc(og_handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		state.fileserverhits.Add(1)
+		og_handler.ServeHTTP(w, r)
+	})
+}
+
+// handler method for metrics counter. needs to be a method for the apiconfig struct as a poitner so that we can access the in memory counter variable when the handler is used.
+func (state *apiConfig) metricshandler(reswriter http.ResponseWriter, webrequest *http.Request) {
+	val := state.fileserverhits.Load()
+	reswriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	reswriter.WriteHeader(200)
+	reswriter.Write([]byte(fmt.Sprintf("Hits: %d", val)))
+}
+
+// handler method for reseting metric coutner back to 0. needs to be a method for the apiconfig struct as a poitner so that we can access the in memory counter variable when the handler is used.
+func (state *apiConfig) resetmetricshandler(reswriter http.ResponseWriter, webrequest *http.Request) {
+	state.fileserverhits.Store(0)
+	reswriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	reswriter.WriteHeader(200)
+	reswriter.Write([]byte("Metrics counter has been reset to 0."))
+}
+
+// handler setup for healthz to determine if website is good.
+func healthzhandler(reswriter http.ResponseWriter, webrequest *http.Request) {
 	reswriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	reswriter.WriteHeader(200)
 	reswriter.Write([]byte("OK"))
 }
 
 func main() {
+	// initialize in memory tracker
+	state := &apiConfig{}
+
+	// ServeMux is an HTTP request multiplexer. It matches the URL of each incoming request against a list of registered patterns and calls the handler for the pattern that most closely matches the URL.
 	mux := http.NewServeMux()
 	port := "8080"
 	server := &http.Server{
@@ -19,11 +54,19 @@ func main() {
 		Handler: mux,
 	}
 
-	// Register hanlder for standing serving files for webpage
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	// Registers route handling for /app, which will serve files for webpage. Note: mux.Handle requires an http.handler where mux.handlefunc converts the handle function to a http.Handler under the hood.
+	apphandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", state.middlewareMetricsInc(apphandler))
 
-	//Register /healthz handler. Handling /healthz requests to make sure page is website is ready
-	mux.HandleFunc("/", myhandler)
+	// Registers /healthz route for telling if webpage is ready
+	mux.HandleFunc("/", healthzhandler)
 
+	// Registes /metrics route for getting data on webpage visit number
+	mux.HandleFunc("/metrics/", state.metricshandler)
+
+	// Registers /reset route for setting metrics data back to 0
+	mux.HandleFunc("/reset/", state.resetmetricshandler)
+
+	// call that starts your HTTP server and keeps it running, continuously listening for incoming HTTP requests.
 	server.ListenAndServe()
 }
